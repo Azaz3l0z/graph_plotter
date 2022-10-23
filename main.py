dc03570ecbb48a7525b810921b078aa506dd474a
DheltaHalo <yagohernandez2001@gmail.com>
2022-10-23 20:46:04 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import urllib.parse
+
+n = 5
+
+x = np.random.random_sample(n)
+y = np.random.random_sample(n)
+
+# Calculate error
+formula = "x*2 + 1/x + ln(x^3)"
+
+variables = {
+    "x": 0.1,
+    "y": 0.1
+}
+
+requests_input = "x+3=1"
+derivate = 
+
+
+print(urllib.parse.quote_plus(requests_input))
+
+# for x_n in variables:
+    
+
+
+# print(x, y)   
+
+# plt.plot(x, y, color="red", marker=".", linestyle="-")
+# plt.xlabel("$\infty$")
+# plt.ylabel("Lol2")
+# plt.show()
