a1a455d0a89cacf16831f66cb5c1df08cb842d38
Azaz3l0z <azaz3l0z@protonmail.com>
2022-10-23 23:16:04 +0200

    fix

--- main.py
+++ main.py
@@ -1,25 +1,83 @@
+import json
+import base64
+import requests
 import numpy as np
 import matplotlib.pyplot as plt
-import urllib.parse
+import urllib.parse as urlparse
 
-n = 5
+def request_derivative(equation: str, variables: dict):
+    wolfree_url = "" +\
+        "https://api.wolframalpha.com/v2/query?"+\
+        "&callback=jQuery36108552903897608751_1666553041562"+\
+        "&output=json"+\
+        "&reinterpret=true"+\
+        "&scantimeout=30"+\
+        "&podtimeout=30"+\
+        "&formattimeout=30"+\
+        "&parsetimeout=30"+\
+        "&totaltimeout=30"+\
+        "&podstate=Step-by-step%20solution"+\
+        "&podstate=Step-by-step"+\
+        "&podstate=Show%20all%20steps"+\
+        "&i2d=true"+\
+        "&input={request_input}"+\
+        "&appid=H9V325-HTALUWHKGK"+\
+        "&_=1666553041563"
+    
+    request_structure = '[{"t":0,"v":"sqrt("},'
+    
+    for var in variables:
+        error = variables[var]["error"]
+        
+        request_structure += f'{{"t":3,"c":[[{{"t":0,"v":"("}},{{"t":48,"c":[[{{"t":0,"v":"{equation}"}}],[{{"t":0,"v":"x"}}]]}},{{"t":0,"v":"*{error})"}}],[{{"t":0,"v":"2"}}]]}},'
+        if var != list(variables.keys())[-1]:
+            request_structure += '{"t":0,"v":"+"},'
+    
+    request_structure += '{"t":0,"v":")"}'
+    
+    for var in variables:
+        value = variables[var]["value"]
+        request_structure += f',{{"t":0,"v":",{var}={value}"}}'
+    
+    request_structure += ']'
+    
+    request_input = base64.b64encode(bytes(request_structure, 'utf-8'))
+    request_input = urlparse.quote_plus(request_input)
+    
+    print(request_input)
 
-x = np.random.random_sample(n)
-y = np.random.random_sample(n)
+    r = requests.get(wolfree_url.format(request_input=request_input))
+    data = r.text[:-2]
+    data = data.replace(urlparse.parse_qs(urlparse.urlparse(r.url).query)["callback"][0], "")
+    data = data[1:]
+    
+    data = json.loads(data)
+    solution = ""
+    for pod in data["queryresult"]["pods"]:
+        if pod["title"] == "Substitution":
+            solution = pod["subpods"][0]["plaintext"]
+            break
+    
+    return solution
+    
+    
+def main():
+    equation = "x + y + ln(x/y)^2"
+    variables = {
+        "x": {
+            "value": 1,
+            "error": 0.1
+        },
+        "y": {
+            "value": 1,
+            "error": 0.1
+        },
+    }
+    solution = request_derivative(equation, variables)
+    print(solution)
 
-# Calculate error
-formula = "x*2 + 1/x + ln(x^3)"
-
-variables = {
-    "x": 0.1,
-    "y": 0.1
-}
-
-requests_input = "x+3=1"
-derivate = 
-
-
-print(urllib.parse.quote_plus(requests_input))
+if __name__ == "__main__":
+    main()
 
 # for x_n in variables:
     
