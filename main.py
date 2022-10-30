Corrected error calculation mistake

main.py
@@ -10,31 +10,33 @@ from evaluate_solutions import eval_all
 def main():
     # Declare equations and variables
     n = np.linspace(0.1, 2, 5)
-    equation = "pi*x + x^2 +ln(y)"
+    equation = "4*pi^2/((2*pi/t)^2 + (b/2)^2)"
     variables = {
-        "x": {
-            "value": n,
-            "error": 0.1
+        "t": {
+            "value": [0.02255,0.02460,0.02730,0.02970,0.03120,0.03285],
+            "error": 4*10**-5
         },
-        "y": {
-            "value": n,
-            "error": 0.1
+        "b": {
+            "value": [50, 45, 15.7, 12.6, 16.4, 25.7],
+            "error": [6, 4, 0.9, 0.8, 0.2, 1.5]
         },
     }
+    
+    h = [30, 40, 50, 60, 70, 80]
     
     # Get the solutions from wolfram alpha and evaluate them
     solution = make_requests(equation, variables)
     function_values = eval_all(solution)
     
     # Set an x and y for plotting
-    x = variables["x"]["value"]
+    x = h
     y = [x.value() for x in function_values]
     err_y = [x.error() for x in function_values]
 
     # Fit data linearly and taking errors into account (weight)
     coeff, covariance = np.polyfit(x, y, 1, w=err_y, cov=True)
     fit = np.poly1d(coeff)
-    fit_error = [np.sqrt(covariance[i, i]) for i in range(fit.order + 1)]
+    fit_error = np.sqrt(np.diag(covariance))
     
     # Get R Squared
     # fit values, and mean
@@ -50,16 +52,16 @@ def main():
     txt = ""
     txt += f'Your fit is: {str(fit).replace(chr(10), "")}'
     for i in range(fit.order + 1):
-        i_error = ScientificErrorNotation(0, fit_error[fit.order - i])
-        txt += f"\n\tError in x^{i}: {i_error.error()}"
+        i_error = ScientificErrorNotation(0, fit_error[i])
+        txt += f"\n\tError in x^{fit.order - i}: {i_error.error()}"
      
     print(txt)
     
     # Plot
     plt.errorbar(x, y, fmt='or', yerr=err_y)
-    plt.scatter(x, fit(x))
-    plt.xlabel("x", fontsize=16)
-    plt.ylabel("F(x,y)", fontsize=16)
+    plt.plot(x, fit(x))
+    plt.xlabel("$h$ / mm", fontsize=16)
+    plt.ylabel("$\\tau_0^2$ / s$^2$", fontsize=16)
     
     x_fit = np.linspace(min(x), max(x), 1000)
     plt.plot(x_fit, fit(x_fit))